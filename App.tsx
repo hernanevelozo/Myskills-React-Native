907ea225d07d54eb8daf944b6e1e8f9809e67f96
hernanevelozo <hernane.rosa@sga.pucminas.br>
2022-07-27 17:29:24 -0300

    Add comentário app.tsx

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Home} from './src/pages/Home';
 import { StatusBar } from "react-native";
 
+//Recebe o componente Home e retorna um componente
 export default function App(){
   return(
     <>
